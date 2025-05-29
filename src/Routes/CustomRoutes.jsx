e5f0ff97f6fa0ab8b6e10d61dd07beda14653136
muzammilnawaz <muzammilnawaz2935337@gmail.com>
2025-05-29 09:59:26 -0700

    add code

--- src/Routes/CustomRoutes.jsx
+++ src/Routes/CustomRoutes.jsx
@@ -3,7 +3,35 @@ import React from 'react'
 const CustomRoutes = () => {
   return (
     <div>
-      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aut, exercitationem quo eum tempora odio obcaecati labore eligendi placeat quam, praesentium quos aspernatur neque voluptatem doloribus corrupti, maiores quia? Nulla, porro?</p>
+        <div className='h-screen bg-gray-700'>
+
+        </div>
+        <div className='min:h-screen bg-gray-900 flex justify-center items-center flex-wrap gap-y-10 py-5 px-10  gap-x-10'>
+    <div className='w-[300px] bg-gray-600 border-2 rounded-lg border-red-500 font-mono px-5 py-5 text-center text-white'>
+        <h1>Helo react</h1>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aut, exercitationem quo eum tempora odio obcaecati labore eligendi placeat quam, praesentium quos aspernatur neque voluptatem doloribus corrupti, maiores quia? Nulla, porro?</p>
+      </div>
+    <div className='w-[300px] bg-gray-600 border-2 rounded-lg border-red-500 font-mono px-5 py-5 text-center text-white'>
+        <h1>Helo react</h1>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aut, exercitationem quo eum tempora odio obcaecati labore eligendi placeat quam, praesentium quos aspernatur neque voluptatem doloribus corrupti, maiores quia? Nulla, porro?</p>
+      </div>
+    <div className='w-[300px] bg-gray-600 border-2 rounded-lg border-red-500 font-mono px-5 py-5 text-center text-white'>
+        <h1>Helo react</h1>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aut, exercitationem quo eum tempora odio obcaecati labore eligendi placeat quam, praesentium quos aspernatur neque voluptatem doloribus corrupti, maiores quia? Nulla, porro?</p>
+      </div>
+    <div className='w-[300px] bg-gray-600 border-2 rounded-lg border-red-500 font-mono px-5 py-5 text-center text-white'>
+        <h1>Helo react</h1>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aut, exercitationem quo eum tempora odio obcaecati labore eligendi placeat quam, praesentium quos aspernatur neque voluptatem doloribus corrupti, maiores quia? Nulla, porro?</p>
+      </div>
+    <div className='w-[300px] bg-gray-600 border-2 rounded-lg border-red-500 font-mono px-5 py-5 text-center text-white'>
+        <h1>Helo react</h1>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aut, exercitationem quo eum tempora odio obcaecati labore eligendi placeat quam, praesentium quos aspernatur neque voluptatem doloribus corrupti, maiores quia? Nulla, porro?</p>
+      </div>
+    <div className='w-[300px] bg-gray-600 border-2 rounded-lg border-red-500 font-mono px-5 py-5 text-center text-white'>
+        <h1>Helo react</h1>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aut, exercitationem quo eum tempora odio obcaecati labore eligendi placeat quam, praesentium quos aspernatur neque voluptatem doloribus corrupti, maiores quia? Nulla, porro?</p>
+      </div>
+    </div>
     </div>
   )
 }
