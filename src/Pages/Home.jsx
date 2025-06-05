a6a335d3a1acd3c0c3e45664e5e6e0d665838b79
muzammilnawaz <muzammilnawaz2935337@gmail.com>
2025-06-05 09:56:04 -0700

    add new code

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -6,6 +6,7 @@ const Home = () => {
       <h1 className="font-poppins" >Welcome to the Home Page</h1>
       <p>This is the main page of Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet magni id nemo qui sed doloribus culpa, vitae dolores tempora accusamus asperiores iure excepturi quidem earum possimus! Qui est deleniti voluptate. our application.</p>
       <p>Feel free to explore the features and functionalities.</p>
+   
     </div>
   )
 }
