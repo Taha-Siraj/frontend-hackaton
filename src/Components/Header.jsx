d8e3ece82df97265f5a12c822c35ccee7a7b0204
Taha Siraj <Pikachugaming899@gmail.com>
2025-07-08 05:32:40 -0700

    app

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
   return (
-    <div>
-        <h1>Header</h1>
+    <div className='flex justify-center border-l-gray-950 text-white'>
+     <header className='fixed z-50 flex justify-center items-center gap-x-10 text-xl rounded-2xl  h-[50px] w-[400px] '>
+    <Link to={'/'} >Home</Link>    
+    <Link to={'/login'} >Login</Link>    
+    <Link to={'/signup'} > Signup</Link>    
+    </header>
     </div>
   )
 }
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>
+    <div className='bg-gray-900 h-screen  text-white'>
       <h1>Home Page</h1>
       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam alias distinctio veniam porro nihil eum praesentium cumque earum aliquid consequatur necessitatibus molestiae quis, eius quaerat perferendis est veritatis quos quisquam?</p>
     </div>
